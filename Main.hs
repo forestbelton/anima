d9d880a1b6ebe8738231b3b5f9d1b54e59807bf8
Forest Belton <forest@homolo.gy>
2014-12-27 02:31:49 +0000

    Add REPL

--- Main.hs
+++ Main.hs
@@ -4,11 +4,31 @@ import Anima.Types
 import Anima.Eval
 import Anima.Parser
 
-eval :: Expr -> IO ()
-eval e = case typeOf [] e of
-    Nothing -> putStrLn "Failed to typecheck"
-    _       -> print $ beta e
+import Control.Monad
+import Text.ParserCombinators.Parsec
+import System.IO
+
+banner :: String
+banner =  "   __    _  _  ____  __  __    __     \n"
+       ++ "  /__\\  ( \\( )(_  _)(  \\/  )  /__\\    \n"
+       ++ " /(__)\\  )  (  _)(_  )    (  /(__)\\   \n"
+       ++ "(__)(__)(_)\\_)(____)(_/\\/\\_)(__)(__)  Version 0.1\n"
 
 main :: IO ()
 main = do
-    eval (EApp (eID TUnit) unit)
+    putStrLn banner
+    repl
+
+repl :: IO ()
+repl = forever $ do
+    hSetBuffering stdout NoBuffering
+    putStr "> "
+    line <- getLine
+    case parse expr "" line of
+        Right e -> do
+            case typeOf [] e of
+                Just ty ->
+                    putStrLn $ line ++ " : " ++ (show ty)
+                Nothing ->
+                    putStrLn "Failed to typecheck"
+        Left err   -> print err
--- README.md
+++ README.md
@@ -2,3 +2,23 @@ anima [![status](https://secure.travis-ci.org/forestbelton/anima.png)](http://tr
 ===
 
 A simple dependently-typed programming language
+
+example
+-------
+
+```bash
+$ cabal run
+Preprocessing executable 'anima' for anima-0.1.0.0...
+   __    _  _  ____  __  __    __
+  /__\  ( \( )(_  _)(  \/  )  /__\
+ /(__)\  )  (  _)(_  )    (  /(__)\
+(__)(__)(_)\_)(____)(_/\/\_)(__)(__)  Version 0.1
+
+> E
+E : TUnit
+> (lam TE E)
+(lam TE E) : EPi TUnit TUnit
+> (lam TE 0)
+(lam TE 0) : EPi TUnit TUnit
+>
+```
