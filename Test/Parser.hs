cb796de2ab74ae22347339238f605257fd83f9a4
Forest Belton <fbelton@theore.tc>
2014-12-29 23:14:11 -0800

    Add more tests for parsing booleans

--- Test/Parser.hs
+++ Test/Parser.hs
@@ -14,7 +14,9 @@ tests =
     , testCase "can parse unit type" parseUnitTy
     , testCase "can parse type type" parseTyTy
     , testCase "can parse function () -> ()" parseIdFunc
-    , testCase "can parse true" parseTrueFunc
+    , testCase "can parse true" parseTrue
+    , testCase "can parse false" parseFalse
+    , testCase "can parse bool type" parseBoolTy
     ]
 
 instance Eq ParseError where
@@ -24,10 +26,10 @@ parser_test :: Term -> String -> Assertion
 parser_test t s = case (Right t ~=? parse expr "" s) of
     TestCase as -> as
 
-parseUnit      = parser_test (Base Unit) "Unit"
-parseUnitTy    = parser_test (Base TUnit) "TUnit"
-parseTyTy      = parser_test (Base Type) "Type"
-parseIdFunc    = parser_test (Binder Lam (Base TUnit) (Base Unit)) "(lam TUnit Unit)"
-parseTrueFunc  = parser_test (Base ATrue) "True"
-parseFalseFunc = parser_test (Base AFalse) "False"
-parseBoolFunc  = parser_test (Base TBool) "Bool"
+parseUnit    = parser_test (Base Unit) "Unit"
+parseUnitTy  = parser_test (Base TUnit) "TUnit"
+parseTyTy    = parser_test (Base Type) "Type"
+parseIdFunc  = parser_test (Binder Lam (Base TUnit) (Base Unit)) "(lam TUnit Unit)"
+parseTrue    = parser_test (Base ATrue) "True"
+parseFalse   = parser_test (Base AFalse) "False"
+parseBoolTy  = parser_test (Base TBool) "Bool"
